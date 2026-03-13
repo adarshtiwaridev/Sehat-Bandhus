63129aab864f3031b0aee2460f9aba830ac9d20a
akansha2006123 <akanshanov.2006@gmail.com>
2026-03-13 16:32:57 +0530

    change in line 8

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,11 +1,11 @@
 "use client";
-
+// This is a error file
 import React from "react";
 
 export default function Error({ error, reset }) {
   return (
     <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h2>Something went wrong 😵</h2>
+      <h2>Something went wrong it is  😵</h2>
       <p>{error?.message}</p>
       <button
         onClick={() => reset()}
